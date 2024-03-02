d41cb675350de40eb76ab9985c6ea1aa2a44a50d
Gabriel José <105285297+eoGabrielo@users.noreply.github.com>
2024-03-02 15:57:49 -0300

    numberRandom

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+numSorteado = random.randint(1,10)
+numeroUsuario = 0
+
+def contraCaracter():
+    while True:
+        try:
+            return int(input("Adivinhe o numero sorteado de 1 a 10: "))
+        except ValueError:
+            print("ATENÇÃO - Valor invalido!")
+
+def contraCaracterSecond():
+    while True:
+        try:
+            return int(input("Tente novamente: "))
+        except ValueError:
+            print("ATENÇÃO - Valor invalido!")
+
+numeroUsuario = contraCaracter()
+
+while numeroUsuario != numSorteado:
+
+    if numeroUsuario <= 10:
+        print(f"Não é o numero {numeroUsuario}!")
+        numeroUsuario = contraCaracterSecond()
+    elif numeroUsuario > 10:
+        print(f"Numero {numeroUsuario} é maior que 10, escolha entre 1 a 10!")
+        numeroUsuario = contraCaracterSecond()
+
+
+print(f"ACERTOU!!!, numero {numSorteado} era o sorteado, PARABÉNS!!!")
